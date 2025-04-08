0d2ba0e4ab3ec53d1bc5bcddf188c4836d50ec7e
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-04-08 10:28:00 -0300

    commit

--- PI_TMS.API/PI_TMS.API/obj/Debug/net9.0/PI_TMS.API.AssemblyInfo.cs
+++ PI_TMS.API/PI_TMS.API/obj/Debug/net9.0/PI_TMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI_TMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13697d87dbc7ba3e9a27b025f2528fb2802b507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02d5e38985462714f79c8361e973eb865c3879d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI_TMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI_TMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
