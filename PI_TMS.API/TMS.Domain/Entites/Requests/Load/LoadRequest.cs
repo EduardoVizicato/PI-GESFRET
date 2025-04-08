4cbb7b9b4ff32c0ec36d8a81f06a576145a7a176
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-04-07 21:16:09 -0300

    commit

---
+++ PI_TMS.API/TMS.Domain/Entites/Requests/Load/LoadRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Domain.Entites.Requests.Load
+{
+    internal class LoadRequest
+    {
+        public string Description { get; set; }
+        public float Quantity { get; set; }
+        public string Type { get; set; }
+        public Guid ClientGuid { get; set; }
+    }
+}
---
+++ PI_TMS.API/TMS.Domain/Entites/Requests/Travel/TravelRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Domain.Entites.Requests.Travel
+{
+    internal class TravelRequest
+    {
+        public string TravelName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string DepartureLocation { get; set; }
+        public string ArrivalLocation { get; set; }
+        public float Weight { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+
+        public Guid LoadGuid { get; set; }
+    }
+}
