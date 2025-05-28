17086c1892b2ebbb17bd0cf247a92b6bb719f44b
VitorFanelli <vitorhugofinocencio@gmail.com>
2025-05-28 12:21:19 -0300

    commit

--- PI_TMS.UI/src/app/pages/register/loads/loads.component.ts
+++ PI_TMS.UI/src/app/pages/register/loads/loads.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from "../../../shared/sidebar/sidebar.component";
+import { HttpClientModule } from '@angular/common/http';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LoadService } from './Services/load.service';
+import { load } from './models/load.model';
 
 @Component({
   selector: 'app-loads',
@@ -8,6 +13,32 @@ import { SidebarComponent } from "../../../shared/sidebar/sidebar.component";
   templateUrl: './loads.component.html',
   styleUrl: './loads.component.css'
 })
-export class LoadsComponent {
+
+export class LoadsComponent implements OnInit {
+
+  load: load[] = [];
+
+  constructor(private loadServive: LoadService){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllLoad()
+  }
+
+  getAllLoad(){
+    this.loadServive.getAllLoad().subscribe(
+      (response) => {
+        console.log(response);
+        this.load = response;
+      },
+      (error) => {
+        console.error('Error fetching loads:', error)
+      }
+      
+      
+
+    );
+  }
 
 }
