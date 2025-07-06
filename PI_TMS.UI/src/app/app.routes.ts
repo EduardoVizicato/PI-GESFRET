11a881b46b4e6fc0a7d03347c8a2619597c11eff
felipegalati <felipegalati20@gmail.com>
2025-07-06 15:35:53 -0300

    fix

--- PI_TMS.UI/src/app/app.routes.ts
+++ PI_TMS.UI/src/app/app.routes.ts
@@ -6,26 +6,18 @@ import { TrucksComponent } from './pages/register/trucks/trucks.component';
 import { LoadsComponent } from './pages/register/loads/loads.component';
 import { ClientsComponent } from './pages/register/clients/clients.component';
 import { authGuard } from './_guard/auth.guard';
-<<<<<<< HEAD
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-=======
 import { UserComponent } from './sign-up/user/user.component';
 import { EnterpriseComponent } from './sign-up/enterprise/enterprise.component';
 import { FreightCalculationComponent } from './pages/freight-calculation/freight-calculation.component';
->>>>>>> main
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
-<<<<<<< HEAD
     {path: 'home', component: LandingPageComponent},
-    {path: 'signUp', component: SignUpComponent},
-=======
+    {path: 'login', component: LoginComponent},
     {path: 'signUp-user', component: UserComponent},
     {path: 'signUp-enterprise', component: EnterpriseComponent},
->>>>>>> main
     {path: 'dashboard', component: DashboardComponent, canActivate:[authGuard]},
     {path: 'travels', component: TravelsComponent,canActivate:[authGuard]},
     {path: 'register/trucks', component: TrucksComponent,canActivate:[authGuard]},
