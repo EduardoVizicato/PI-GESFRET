b3daab8052193971af563d7dc36f9ba1852814af
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-10 21:40:22 -0300

    add pagination on loads and change pagination method

--- PI_TMS.UI/src/app/pages/register/trucks/trucks.component.ts
+++ PI_TMS.UI/src/app/pages/register/trucks/trucks.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder } from '@angul
 import { CommonModule } from '@angular/common';
 import { TruckService } from './Services/truck.service';
 import { Truck } from './models/truck.model';
-import { ActivatedRoute, Router, RouterModule,  } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, } from '@angular/router';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { PlateFormatPipe } from "./utils/plate-format.pipe";
 import { EventService } from '../../../shared/service/event.service';
@@ -18,22 +18,18 @@ interface VehicleTypeOption {
 
 @Component({
   selector: 'app-trucks',
-  imports: [SidebarComponent, HttpClientModule, FormsModule, CommonModule, ReactiveFormsModule, NgxMaskDirective, PlateFormatPipe,NgbPaginationModule],
+  imports: [SidebarComponent, HttpClientModule, FormsModule, CommonModule, ReactiveFormsModule, NgxMaskDirective, PlateFormatPipe, NgbPaginationModule],
   providers: [provideNgxMask()],
   templateUrl: './trucks.component.html',
   styleUrl: './trucks.component.css'
 })
-export class TrucksComponent implements OnInit{
-setPage(page: number) {
-  this.page = page;
-}
+export class TrucksComponent implements OnInit {
 
-  items: any[] = [];
-  page: number = 1 ;
+  page: number = 1;
   pageSize: number = 10;
   trucks: Truck[] = [];
   truckForm: FormGroup;
-  editingTruckId: string | null = null; 
+  editingTruckId: string | null = null;
 
   availableVehicleTypes: VehicleTypeOption[] = [
     { id: 0, name: 'Carro' },
@@ -41,15 +37,18 @@ setPage(page: number) {
     { id: 2, name: 'Carreta' },
   ];
 
-  constructor(private truckService: TruckService, private fb: FormBuilder, private router: Router, private route: ActivatedRoute,private eventService: EventService,) {
+  constructor(private truckService: TruckService, private fb: FormBuilder, private router: Router, private route: ActivatedRoute, private eventService: EventService,) {
     this.truckForm = this.createForm();
-   }
+  }
 
+  setPage(page: number) {
+    this.page = page;
+  }
   ngOnInit(): void {
     this.getAllTrucks();
   }
 
-   getAllTrucks() {
+  getAllTrucks() {
     this.truckService.getAllTrucks().subscribe(
       (response) => {
         console.log(response);
@@ -61,7 +60,7 @@ setPage(page: number) {
     );
   }
 
-  createForm(): FormGroup{
+  createForm(): FormGroup {
     return this.fb.group({
       name: [''],
       vehicleType: [null as number | null],
@@ -78,26 +77,26 @@ setPage(page: number) {
         console.log(response)
         this.getAllTrucks();
 
-      const modalElement = document.getElementById('addTruckModal');
-      if (modalElement) {
-        const modalInstance = (window as any).bootstrap.Modal.getInstance(modalElement);
-        if (modalInstance) {
-          modalInstance.hide();
-        } //else {
-        //   se getInstance retornar null
-        //   const bsModal = new (window as any).bootstrap.Modal(modalElement);
-        //   bsModal.hide();
-        // }
-      }
-      this.truckForm.reset();
+        const modalElement = document.getElementById('addTruckModal');
+        if (modalElement) {
+          const modalInstance = (window as any).bootstrap.Modal.getInstance(modalElement);
+          if (modalInstance) {
+            modalInstance.hide();
+          } //else {
+          //   se getInstance retornar null
+          //   const bsModal = new (window as any).bootstrap.Modal(modalElement);
+          //   bsModal.hide();
+          // }
+        }
+        this.truckForm.reset();
 
       },
       error: (err) => this.eventService.showError('Erro inesperado.')
     })
   }
 
-   openEditModal(truck: Truck): void {
-    this.editingTruckId = truck.id; 
+  openEditModal(truck: Truck): void {
+    this.editingTruckId = truck.id;
     this.truckForm.patchValue(truck);
   }
 
@@ -110,17 +109,17 @@ setPage(page: number) {
         this.getAllTrucks();
         const modalElement = document.getElementById('editTruckModal');
         if (modalElement) {
-        const modalInstance = (window as any).bootstrap.Modal.getInstance(modalElement);
-        if (modalInstance) {
-          modalInstance.hide();
-        } //else {
-        //   se getInstance retornar null
-        //   const bsModal = new (window as any).bootstrap.Modal(modalElement);
-        //   bsModal.hide();
-        // }
-      }
+          const modalInstance = (window as any).bootstrap.Modal.getInstance(modalElement);
+          if (modalInstance) {
+            modalInstance.hide();
+          } //else {
+          //   se getInstance retornar null
+          //   const bsModal = new (window as any).bootstrap.Modal(modalElement);
+          //   bsModal.hide();
+          // }
+        }
         this.truckForm.reset();
-        this.editingTruckId = null; 
+        this.editingTruckId = null;
       },
       error: (err) => this.eventService.showError('Erro inesperado.')
     });
