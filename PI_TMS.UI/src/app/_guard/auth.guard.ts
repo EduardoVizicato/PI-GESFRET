3d1ff7eb334f5abaf1982519791cd9b628dc7e63
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-12 21:11:38 -0300

    create expiration at token

--- PI_TMS.UI/src/app/_guard/auth.guard.ts
+++ PI_TMS.UI/src/app/_guard/auth.guard.ts
@@ -3,15 +3,16 @@ import { inject } from '@angular/core';
 import { AuthTokenService } from './service/auth-token.service';
 
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const tokenService = inject(AuthTokenService);
-  const JsonToken = (tokenService.decodePayloadJWT());
-  // console.log(JsonToken);
-  if (JsonToken) {
+  const authTokenService = inject(AuthTokenService);
+  const tokenPayload = authTokenService.decodePayloadJWT();
+
+  if (tokenPayload && !authTokenService.isTokenExpired()) {
     return true;
   }
+  console.warn('Acesso negado: Token inválido ou expirado');
   const router = inject(Router);
   router.navigate(['/login']);
   return false;
-  
+
 };
 
