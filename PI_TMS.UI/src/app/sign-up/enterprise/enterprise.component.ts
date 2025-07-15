45bd1f93d3d4ce68897c38d1019b459624050cbb
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-14 23:10:09 -0300

    Add verification on enterprise page

--- PI_TMS.UI/src/app/sign-up/enterprise/enterprise.component.ts
+++ PI_TMS.UI/src/app/sign-up/enterprise/enterprise.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxMaskDirective } from 'ngx-mask';
+import { enterprise } from './model/enterprise.model';
 
 @Component({
   selector: 'app-enterprise',
@@ -11,9 +12,29 @@ import { NgxMaskDirective } from 'ngx-mask';
   styleUrl: './enterprise.component.css'
 })
 export class EnterpriseComponent {
-  constructor(private router: Router) { }
-  
+
+  enterpriseForm: FormGroup;
+  enterprises: enterprise[] = [];
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.enterpriseForm = this.createForm();
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      taxId: this.fb.group({
+        taxId: ['', Validators.required, Validators.pattern(/^\d{11}$/)] 
+      }),
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
   onSubmit(mainContainer: HTMLElement): void {
+    if (this.enterpriseForm.invalid) {
+      console.log('Formulário inválido');
+      this.enterpriseForm.markAllAsTouched(); // mostra erros
+      return;
+    }
     mainContainer.classList.add('active');
     setTimeout(() => {
       this.router.navigate(['/signUp-user']);
---
+++ PI_TMS.UI/src/app/sign-up/enterprise/model/enterprise.model.ts
@@ -0,0 +1,7 @@
+export interface enterprise {
+    name: string;
+    taxId: {
+        taxId: string;
+    }
+    email: string;
+}
