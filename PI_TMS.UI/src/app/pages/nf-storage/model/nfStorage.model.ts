8994472a18201d40f08fc22afe85ab406a659c39
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-18 20:15:16 -0300

    nfStorage working with pdfViewer

---
+++ PI_TMS.UI/src/app/pages/nf-storage/model/nfStorage.model.ts
@@ -0,0 +1,3 @@
+export interface nfStorage {
+    path: string;
+}
--- PI_TMS.UI/src/app/pages/nf-storage/nf-storage.component.ts
+++ PI_TMS.UI/src/app/pages/nf-storage/nf-storage.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { nfStorage } from './model/nfStorage.model';
+
 
 @Component({
   selector: 'app-nf-storage',
-  imports: [SidebarComponent, BrowserModule, PdfViewerModule],
+  imports: [SidebarComponent, CommonModule,PdfViewerModule],
   templateUrl: './nf-storage.component.html',
   styleUrl: './nf-storage.component.css'
 })
 export class NfStorageComponent {
-  pdfSrc: any = null;
+  link: [string][] = [
+    [ '/pdf/teste1.pdf'],
+    [ '/pdf/teste2.pdf'],
+    [ '/pdf/teste3.pdf'],
+    [ '/pdf/teste4.pdf']
+  ];
+  nf: nfStorage[] = [];
+
+  constructor() {
+    this.nf = this.link.map((item) => ({ path: item[0] }));
+  }
+
+  
 
 }
