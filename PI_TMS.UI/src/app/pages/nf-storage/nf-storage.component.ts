2dd0e4c9595187001a016d42ec6370efcff9e6e7
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-17 22:57:58 -0300

    try use pdfViewr

--- PI_TMS.UI/src/app/pages/nf-storage/nf-storage.component.ts
+++ PI_TMS.UI/src/app/pages/nf-storage/nf-storage.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-nf-storage',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, BrowserModule, PdfViewerModule],
   templateUrl: './nf-storage.component.html',
   styleUrl: './nf-storage.component.css'
 })
 export class NfStorageComponent {
+  pdfSrc: any = null;
 
 }
