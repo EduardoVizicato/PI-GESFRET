dd5e573f7b9cece10e15a1661a25c5a09e4fdd15
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-07-28 15:59:36 -0300

    conecting userInfo with back

--- PI_TMS.UI/src/app/pages/user-info/user-info.component.ts
+++ PI_TMS.UI/src/app/pages/user-info/user-info.component.ts
@@ -3,38 +3,67 @@ import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
 import { AuthTokenService } from '../../_guard/service/auth-token.service';
 import { UserInfoService } from './service/user-info.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserInfo } from './models/userInfo.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
-  imports: [SidebarComponent, CommonModule, FormsModule],
+  imports: [SidebarComponent, CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.css'
 })
 export class UserInfoComponent {
-   user: UserInfo | null = null;
+  user: UserInfo | null = null;
+  userForm: FormGroup;
 
-
-  constructor(private authTokenService: AuthTokenService, private userInfoService: UserInfoService) {
-
+  constructor(private authTokenService: AuthTokenService, private userInfoService: UserInfoService, private fb: FormBuilder,) {
+    this.userForm = this.createForm();
   }
   ngOnInit(): void {
+
     this.getUser();
+  }
+  createForm(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      taxId: this.fb.group({
+        taxId: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]]
+      }),
+      phone: ['', [Validators.required, Validators.pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/)]],
+      // Adicione outros campos conforme sua interface UserInfo
+    });
   }
   getUser() {
     const userId = this.authTokenService.getUserId();
     if (userId) {
       this.userInfoService.getUserbyId(userId).subscribe(
-        (response: UserInfo) => {
-          console.log(response);
-          this.user = response;
+        (user: UserInfo) => {
+          console.log("Usuário obtido:", user);
+          this.userForm.patchValue({
+            name: user.firstName,
+            surname: user.lastName,
+            taxId: {
+              taxId: user.taxId.taxId
+            },
+            phone: user.phoneNumber,
+            email: user.email,
+            // outros campos que quiser preencher
+          });
         },
         (error) => {
           console.error("Erro ao buscar o usuário:", error);
         }
       );
-    } 
+    }
   }
-
+  onSubmit() {
+    if (this.userForm.valid) {
+      const updatedUser = this.userForm.value;
+      console.log('Usuário atualizado:', updatedUser);
+      // Pode chamar um this.userInfoService.updateUser(updatedUser) aqui, por exemplo
+    }
+  }
 }
