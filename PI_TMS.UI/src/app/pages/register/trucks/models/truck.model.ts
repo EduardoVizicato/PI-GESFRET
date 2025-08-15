171d69c4d236923044eaa16bfb3dc16d0b20e02d
felipegalati <felipegalati20@gmail.com>
2025-08-15 08:49:14 -0300

    truck modal

--- PI_TMS.UI/src/app/pages/register/trucks/models/truck.model.ts
+++ PI_TMS.UI/src/app/pages/register/trucks/models/truck.model.ts
@@ -1,9 +1,11 @@
 export interface Truck {
     id: string;
     name: string;
-    // vehicleType: number;
     vehicleRegistrationPlate: {
         registrationPlate: string;
     };
     userId: string;
+    truckType?: string;
+    wheelType?: string;
+    bodyType?: string;
 }
--- PI_TMS.UI/src/app/pages/register/trucks/trucks.component.ts
+++ PI_TMS.UI/src/app/pages/register/trucks/trucks.component.ts
@@ -1,7 +1,6 @@
-// Importe AfterViewInit e OnDestroy para gerenciar o ciclo de vida do modal
-import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TruckService } from './Services/truck.service';
 import { Truck } from './models/truck.model';
@@ -10,13 +9,9 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { PlateFormatPipe } from "./utils/plate-format.pipe";
 import { EventService } from '../../../shared/service/event.service';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 
 declare var bootstrap: any;
-
-interface VehicleTypeOption {
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'app-trucks',
@@ -25,8 +20,7 @@ interface VehicleTypeOption {
   templateUrl: './trucks.component.html',
   styleUrl: './trucks.component.css'
 })
-
-export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
+export class TrucksComponent implements OnInit, OnDestroy {
 
   searchTerm: string = '';
   page: number = 1;
@@ -34,14 +28,29 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
   trucks: Truck[] = [];
   truckForm: FormGroup;
   editingTruckId: string | null = null;
+  private subscriptions: Subscription[] = [];
 
   private addTruckModal: any;
   private editTruckModal: any;
 
-  availableVehicleTypes: VehicleTypeOption[] = [
-    { id: 0, name: 'Carro' },
-    { id: 1, name: 'Caminhão' },
-    { id: 2, name: 'Carreta' },
+  truckTypes: string[] = ['Tração', 'Reboque (Carreta)'];
+  
+  wheelTypes: string[] = [
+    'Caminhão truck (3-4 eixos)',
+    'Caminhão toco (2 eixos)',
+    'Cavalo (2-3 eixos e acopla carroceria)',
+    'VAN (2 eixos)',
+    'Utilitários (2 eixos)',
+    'Outros'
+  ];
+
+  bodyTypes: string[] = [
+    'Não aplicável (Tanque, Caçamba, Boiadeira, etc)',
+    'Aberta',
+    'Fechada/Baú',
+    'Granelera',
+    'Porta Container',
+    'Sider'
   ];
 
   constructor(private truckService: TruckService, private fb: FormBuilder, private router: Router, private route: ActivatedRoute, private eventService: EventService,) {
@@ -50,16 +59,61 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnInit(): void {
     this.getAllTrucks();
+    this.setupConditionalLogic();
+  }
+  
+  private setupConditionalLogic(): void {
+    const truckTypeControl = this.truckForm.get('truckType') as FormControl;
+    const wheelTypeControl = this.truckForm.get('wheelType') as FormControl;
+    const bodyTypeControl = this.truckForm.get('bodyType') as FormControl;
+
+    const truckTypeSub = truckTypeControl.valueChanges.subscribe(type => {
+      wheelTypeControl.reset();
+      bodyTypeControl.reset();
+
+      if (type === 'Tração') {
+        this.setValidatorsAndEnable(wheelTypeControl, [Validators.required]);
+        this.clearValidatorsAndDisable(bodyTypeControl);
+      } else if (type === 'Reboque (Carreta)') {
+        this.setValidatorsAndEnable(bodyTypeControl, [Validators.required]);
+        this.clearValidatorsAndDisable(wheelTypeControl);
+      } else {
+        this.clearValidatorsAndDisable(wheelTypeControl);
+        this.clearValidatorsAndDisable(bodyTypeControl);
+      }
+    });
+
+    const wheelTypeSub = wheelTypeControl.valueChanges.subscribe(wheelType => {
+        if (truckTypeControl.value !== 'Tração') return;
+
+        if (wheelType === 'Cavalo (2-3 eixos e acopla carroceria)') {
+            bodyTypeControl.reset();
+            this.clearValidatorsAndDisable(bodyTypeControl);
+        } else {
+            this.setValidatorsAndEnable(bodyTypeControl, [Validators.required]);
+        }
+    });
+    
+    this.subscriptions.push(truckTypeSub, wheelTypeSub);
   }
 
-
+  private setValidatorsAndEnable(control: FormControl, validators: any[]): void {
+    control.setValidators(validators);
+    control.enable();
+    control.updateValueAndValidity();
+  }
+  
+  private clearValidatorsAndDisable(control: FormControl): void {
+    control.clearValidators();
+    control.disable();
+    control.updateValueAndValidity();
+  }
+  
   ngAfterViewInit(): void {
-
     const addModalEl = document.getElementById('addTruckModal');
     if (addModalEl) {
       this.addTruckModal = new bootstrap.Modal(addModalEl);
     }
-
     const editModalEl = document.getElementById('editTruckModal');
     if (editModalEl) {
       this.editTruckModal = new bootstrap.Modal(editModalEl);
@@ -69,6 +123,7 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy(): void {
     this.addTruckModal?.dispose();
     this.editTruckModal?.dispose();
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   getAllTrucks() {
@@ -93,16 +148,19 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
   createForm(): FormGroup {
     return this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      vehicleType: [null as number | null],
       vehicleRegistrationPlate: this.fb.group({
-        registrationPlate: ['', [Validators.required, Validators.pattern(/^[A-Z]{3}[0-9]{4}$|^[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}$/i)
-        ]],
-      })
-    })
+        registrationPlate: ['', [Validators.required, Validators.pattern(/^[A-Z]{3}[0-9]{4}$|^[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}$/i)]],
+      }),
+      truckType: [null, [Validators.required]],
+      wheelType: [{ value: null, disabled: true }], 
+      bodyType: [{ value: null, disabled: true }]
+    });
   }
 
   showAddModal(): void {
     this.truckForm.reset();
+    this.truckForm.get('wheelType')?.disable();
+    this.truckForm.get('bodyType')?.disable();
     this.addTruckModal?.show();
   }
 
@@ -111,22 +169,20 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
       this.truckForm.markAllAsTouched();
       return;
     }
-    const truckData: Truck = this.truckForm.value;
+    const truckData: Truck = this.truckForm.getRawValue();
     this.truckService.addTruck(truckData).subscribe({
       next: (response) => {
         this.getAllTrucks();
-        // fecha aq
         this.addTruckModal?.hide();
         this.truckForm.reset();
       },
       error: (err) => this.eventService.showError('Erro inesperado.')
-    })
+    });
   }
 
   openEditModal(truck: Truck): void {
     this.editingTruckId = truck.id;
     this.truckForm.patchValue(truck);
-    // mostra aq
     this.editTruckModal?.show();
   }
 
@@ -136,13 +192,11 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
       return;
     }
 
-    const updatedTruckData = this.truckForm.value;
+    const updatedTruckData = this.truckForm.getRawValue();
     this.truckService.updateTruck(this.editingTruckId, updatedTruckData).subscribe({
       next: () => {
         this.getAllTrucks();
-
         this.editTruckModal?.hide();
-
         this.truckForm.reset();
         this.editingTruckId = null;
       },
@@ -156,7 +210,7 @@ export class TrucksComponent implements OnInit, AfterViewInit, OnDestroy {
         this.trucks = this.trucks.filter(truck => truck.id !== id);
       },
       error: (err) => this.eventService.showError('Erro inesperado.')
-    })
+    });
   }
 
   setPage(page: number) {
