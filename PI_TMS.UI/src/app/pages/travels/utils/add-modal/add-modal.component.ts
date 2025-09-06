3a661435ccca3cfb1e45c808f7905c3ee3394eb1
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-09-05 21:40:48 -0300

    creating modal

---
+++ PI_TMS.UI/src/app/pages/travels/utils/add-modal/add-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-modal',
+  imports: [],
+  templateUrl: './add-modal.component.html',
+  styleUrl: './add-modal.component.css'
+})
+export class AddModalComponent {
+
+}
