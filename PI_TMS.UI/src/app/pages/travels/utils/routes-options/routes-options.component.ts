b806ffe493dc42b4d2b24ae0f56d911e5debbdbe
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-09-19 10:56:59 -0300

    add mask and try fix bugs

--- PI_TMS.UI/src/app/pages/travels/utils/routes-options/routes-options.component.ts
+++ PI_TMS.UI/src/app/pages/travels/utils/routes-options/routes-options.component.ts
@@ -1,13 +1,27 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ZipCodeData, ZipCodeService } from '../../service/zip-code/zip-code.service';
 import { City, CityService } from '../../service/city/city.service';
 import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { PlateFormatPipe } from '../PlateFormat/plate-format.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-routes-options',
-  imports: [AsyncPipe],
+  imports: [
+    AsyncPipe,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbPaginationModule,
+    CurrencyMaskModule,
+    NgxMaskDirective
+  ],
   templateUrl: './routes-options.component.html',
   styleUrl: './routes-options.component.css'
 })
@@ -50,49 +64,65 @@ export class RoutesOptionsComponent {
     }
   }
 
-  /**
-   * ATUALIZAÇÃO AQUI 👇
-   * O método patchValue foi ajustado para refletir a nova estrutura aninhada, 
-   * onde 'origin' e 'destination' são FormGroups que contêm um FormControl 'city'.
-   */
+
   selectCity(city: City, type: 'origin' | 'destination', event: MouseEvent): void {
     event.preventDefault();
     const cityName = `${city.nome}/${city.estado}`;
 
     if (type === 'origin') {
-      // Modificado para atualizar o controle 'city' dentro do grupo 'origin'
-      this.parentForm.get('route.origin')?.patchValue({ city: cityName });
+      this.parentForm.get('route.origin.city')?.patchValue({ city: cityName });
       this.showSuggestionsOrigin = false;
+      console.log(this.parentForm.get('route.origin.city')?.value);
     } else {
-      // Modificado para atualizar o controle 'city' dentro do grupo 'destination'
-      this.parentForm.get('route.destination')?.patchValue({ city: cityName });
+
+      this.parentForm.get('route.destination.city')?.patchValue({ city: cityName });
       this.showSuggestionsDestination = false;
     }
   }
 
-  searchZipCode(event: Event): void {
+  searchZipCode(event: Event, type: 'origin' | 'destination'): void {
     const input = event.target as HTMLInputElement;
     const zipCode = input.value;
+    if (type === 'origin') {
+      this.zipCodeService.searchZipCode(zipCode).subscribe((data: ZipCodeData | null) => {
+        const routeGroup = this.parentForm.get('route.origin') as FormGroup;
+        if (data) {
+          routeGroup.patchValue({
+            street: data.logradouro,
+            neighborhood: data.bairro,
+            city: data.localidade,
+            state: data.uf,
 
-    this.zipCodeService.searchZipCode(zipCode).subscribe((data: ZipCodeData | null) => {
-      const routeGroup = this.parentForm.get('route') as FormGroup;
-      if (data) {
-        routeGroup.patchValue({
-          street: data.logradouro,
-          neighborhood: data.bairro,
-          city: data.localidade,
-          state: data.uf,
-
-        });
-      } else {
-        routeGroup.patchValue({
-          street: '',
-          neighborhood: '',
-          city: '',
-          state: '',
-        });
-      }
-    });
+          });
+        } else {
+          routeGroup.patchValue({
+            street: '',
+            neighborhood: '',
+            city: '',
+            state: '',
+          });
+        }
+      });
+    } else if (type === 'destination') {
+      this.zipCodeService.searchZipCode(zipCode).subscribe((data: ZipCodeData | null) => {
+        const routeGroup = this.parentForm.get('route.destination') as FormGroup;
+        if (data) {
+          routeGroup.patchValue({
+            street: data.logradouro,
+            neighborhood: data.bairro,
+            city: data.localidade,
+            state: data.uf,
+          });
+        } else {
+          routeGroup.patchValue({
+            street: '',
+            neighborhood: '',
+            city: '',
+            state: '',
+          });
+        }
+      });
+    }
   }
   updateTypeAddress(type: string): void {
     this.selectedAddressType = type;
