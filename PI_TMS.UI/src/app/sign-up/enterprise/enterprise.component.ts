077efea77f74a2f45e0b027f15b514c600903e7a
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2025-11-25 22:38:29 -0300

    Fix Create User

--- PI_TMS.UI/src/app/sign-up/enterprise/enterprise.component.ts
+++ PI_TMS.UI/src/app/sign-up/enterprise/enterprise.component.ts
@@ -6,16 +6,18 @@ import { NgxMaskDirective } from 'ngx-mask';
 import { enterprise } from './model/enterprise.model';
 import { ThemeService } from '../../contrast/theme.service';
 import { EnterpriseService } from './service/enterprise.service';
+import { cnpjValidator } from '../../utils/cnpj.validator';
 
 @Component({
   selector: 'app-enterprise',
   imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxMaskDirective, RouterModule],
   templateUrl: './enterprise.component.html',
-  styleUrl: './enterprise.component.css'
+  styleUrls: ['./enterprise.component.css']
 })
 export class EnterpriseComponent {
 
   enterpriseForm: FormGroup;
+  enterpriseId: enterprise | null = null;
   enterprises: enterprise[] = [];
   constructor(
     private enterpriseService: EnterpriseService,
@@ -30,7 +32,7 @@ export class EnterpriseComponent {
     return this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
       taxId: this.fb.group({
-        taxId: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]]
+        taxId: ['', [Validators.required, cnpjValidator]]
       }),
       email: ['', [Validators.required, Validators.email]]
     });
@@ -46,7 +48,9 @@ export class EnterpriseComponent {
     this.enterpriseService.registerEnterprise(enterpriseData).subscribe({
       next: (response) => {
         console.log('Enterprise registered successfully:', response);
-        this.router.navigate(['/signUp-user']);
+        this.enterpriseId = response; 
+        console.log('Registered Enterprise ID:', this.enterpriseId);
+        this.router.navigate(['/signUp-user'], { state: { enterpriseId: this.enterpriseId } });
       },
       error: (error) => {
         console.error('Error registering enterprise:', error);
